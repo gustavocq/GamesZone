ec74b3eb5e62856979fd51d214381b36f6d9cb87
Vitor Ian <goncalvesvitorian@gmail.com>
2022-12-07 13:46:58 -0300

    criando branch vitor

--- GamesZone/src/app/app-routing.module.ts
+++ GamesZone/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './telas/login/login.component';
 import { CadastroComponent } from './telas/cadastro/cadastro.component';
 import { HomeComponent } from './telas/home/home.component';
 import { NavbarPesquisaComponent } from './telas/navbar-pesquisa/navbar-pesquisa.component';
 
 const routes: Routes = [
-  { path: 'login', component: CadastroComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'navbar', component: NavbarPesquisaComponent },
   { path: 'cadastro', component: CadastroComponent },
   { path: 'home', component: HomeComponent }
