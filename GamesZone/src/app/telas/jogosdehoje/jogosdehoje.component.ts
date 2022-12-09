60a887411525bdc45ec942f5500af115630d8d49
Vitor Ian <goncalvesvitorian@gmail.com>
2022-12-09 16:12:55 -0300

    enviando tela de campeonatos, jogadores, jogos de hoje, equipes

---
+++ GamesZone/src/app/telas/jogosdehoje/jogosdehoje.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jogosdehoje',
+  templateUrl: './jogosdehoje.component.html',
+  styleUrls: ['./jogosdehoje.component.css']
+})
+export class JogosdehojeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ GamesZone/src/app/telas/navbar-league/navbar-league.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-league',
+  templateUrl: './navbar-league.component.html',
+  styleUrls: ['./navbar-league.component.css']
+})
+export class NavbarLeagueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
