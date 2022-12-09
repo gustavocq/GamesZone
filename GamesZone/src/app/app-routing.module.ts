aad1138cd84e8e3cdca884991918f4bf5975a1ad
Gustavo Queiroz <gustavocq2013@gmail.com>
2022-12-08 23:05:52 -0300

    adicionado telas equipes/competicoes/jogadores

--- GamesZone/src/app/app-routing.module.ts
+++ GamesZone/src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { CadastroComponent } from './telas/cadastro/cadastro.component';
 import { HomeComponent } from './telas/home/home.component';
 import { NavbarPesquisaComponent } from './telas/navbar-pesquisa/navbar-pesquisa.component';
 import { ChatComponent } from './telas/chat/chat.component';
+import { CampeonatosComponent } from './telas/campeonatos/campeonatos.component';
+import { JogadoresComponent } from './telas/jogadores/jogadores.component';
+import { EquipesComponent } from './telas/equipes/equipes.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'navbar', component: NavbarPesquisaComponent },
   { path: 'cadastro', component: CadastroComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'chat', component: ChatComponent }
+  { path: 'chat', component: ChatComponent },
+  { path: 'campeonatos', component: CampeonatosComponent },
+  { path: 'jogadores', component: JogadoresComponent },
+  { path: 'equipes', component: EquipesComponent }
 ];
 
 @NgModule({
