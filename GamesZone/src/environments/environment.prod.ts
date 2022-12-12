a7b1ad59d7a0974bafb7b731dc97a50eefc26e52
Leonardo Rezende <leorezende742@hotmail.com>
2022-12-12 09:16:16 -0300

    Update environment.prod.ts

--- GamesZone/src/environments/environment.prod.ts
+++ GamesZone/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl: 'https://us-central1-gamezone-dev.cloudfunctions.net/api/',
   //baseUrl: 'http://localhost:5001/gamezone-dev/us-central1/api',
-  api_key: "AIzaSyCUO5K12VAphhmmvlkNc9lfaE9Qyrncryc"
+  api_key: ""
 };
